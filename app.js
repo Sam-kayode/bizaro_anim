1f86082a6a7564104046d874bbf9aa25d124e5d5
Marvellous Sam-kayode <marvelloussamkayode@gmail.com>
2023-11-07 01:00:37 +0100

    add preloader pug file

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-  res.send('<strong>Hello world</strong>')
+  res.render('index', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description'
+      }
+    }
+  })
 })
 
 app.listen(port, () => {

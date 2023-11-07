a50ce47d99b09d78b8fbb2bcf9c815547cdd8208
Marvellous Sam-kayode <marvelloussamkayode@gmail.com>
2023-11-07 01:42:56 +0100

    add data template to template file

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const port = 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.render('index', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Metadata description'
-      }
-    }
-  })
+  res.render('index')
 })
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
